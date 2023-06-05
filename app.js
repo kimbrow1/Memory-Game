8ba53ac6810c1dd034c5781d0089f1ffa59181d3
Brian Kimbrow <bkimbrow25@gmail.com>
2023-06-05 18:30:57 -0500

    newupdates

--- app.js
+++ app.js
@@ -3,3 +3,21 @@
 let tileContainer = document.querySelector(".tiles");
 let colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal",];
 let colorsPicklist = [...colors, ...colors];
+let tileTotal = colorsPicklist.length;
+
+// Game State
+
+let revealCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+
+// Build of tiles with random index
+for(let i = 0; i < tileTotal; i++) {
+let randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+let color = colorsPicklist[randomIndex] ;
+
+colorsPicklist.splice(randomIndex, 1);
+
+console.log(color);
+}
